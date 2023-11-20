6d6289a4ebc7e0b8d17f9144423370f1cc45d72c
DarsanaBoraian <129121551+DarsanaBoraian@users.noreply.github.com>
2023-11-20 10:42:30 +0000

    Sentry Linked

--- src/screens/Urdu.js
+++ src/screens/Urdu.js
@@ -1,6 +1,7 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Button, Text, SafeAreaView} from 'react-native';
 import React from 'react';
 import LocaleHelper from '../helpers/LocaleHelper';
+import * as Sentry from '@sentry/react-native';
 
 export default function Urdu() {
   LocaleHelper.locale = 'ur';
@@ -8,6 +9,12 @@ export default function Urdu() {
     <SafeAreaView>
       <Text>Urdu</Text>
       <Text>{LocaleHelper.t('howru')}</Text>
+      <Button
+        title="Try!"
+        onPress={() => {
+          Sentry.captureException(new Error('First error'));
+        }}
+      />
     </SafeAreaView>
   );
 }
